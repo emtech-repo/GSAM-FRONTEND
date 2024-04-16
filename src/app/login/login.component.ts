bf456cc6b93eebfe1905957d4874948a906e6db7
Zachary macharia <51830126+machariaz@users.noreply.github.com>
2024-04-16 16:15:43 +0300

    Merge branch 'zachary' of https://github.com/emtech-repo/GSAM-FRONTEND into zachary

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,3 +1,84 @@
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { SharedService } from '../shared.service';
+
+// declare function showSuccessToast(msg: any): any;
+// declare function showDangerToast(msg: any): any;
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css'],
+// })
+// export class LoginComponent implements OnInit {
+//   constructor(private servive: SharedService, private router: Router) {
+//     servive.isAuthenticated = false;
+//   }
+//   changetype: any = true;
+//   visible: any = true;
+//   errorMessage: string = '';
+
+//   UserList: any = [];
+
+//   userEmail: string = '';
+//   userPassword: string = '';
+
+//   response: any = {};
+
+//   ngOnInit(): void {}
+
+//   replacer(i: any, val: any) {
+//     if (i === 'Password') {
+//       return undefined;
+//     } else {
+//       return val;
+//     }
+//   }
+//   viewpassword() {
+//     this.visible = !this.visible;
+//     this.changetype = !this.changetype;
+//   }
+//   login() {
+//     this.servive.loader=true;
+//     console.log('hello')
+//     console.log(this.userEmail);
+//     console.log(this.userPassword);
+//     if(this.userEmail =='Admin@gmail.com' && this.userPassword == '123456'){
+//       showSuccessToast("Admin Login Successfully");
+//       localStorage.setItem('currentUser','23');
+//       this.servive.isAuthenticated = true;
+//       window.location.href='/Dashboard'
+//       // this.router.navigate(['/Dashboard']);
+//       this.servive.loader=false;
+//     }else{
+//       showDangerToast('InValid Credential');
+//       this.servive.loader=false;
+//     }
+    
+ 
+//     var val = { Username: this.userEmail, Password: this.userPassword };
+//     this.servive.getLogin(val).subscribe((res) => {
+//       this.response = res;
+
+//       if (this.response['status_code'] == 100) {
+//         this.UserList = JSON.parse(this.response['message'])[0];
+
+//         localStorage.setItem(
+//           'currentUser',
+//           JSON.stringify(this.UserList, this.replacer)
+//         );
+        
+//         this.servive.isAuthenticated = true;
+//       } else {
+//         console.log(this.response);
+
+//         alert(this.response['message']);
+//       }
+//     });
+//   }
+// }
+
+
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SharedService } from '../shared.service';
@@ -25,7 +106,7 @@ export class LoginComponent implements OnInit {
 
   response: any = {};
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   replacer(i: any, val: any) {
     if (i === 'Password') {
@@ -39,41 +120,33 @@ export class LoginComponent implements OnInit {
     this.changetype = !this.changetype;
   }
   login() {
-    this.servive.loader=true;
-    console.log('hello')
-    console.log(this.userEmail);
-    console.log(this.userPassword);
-    if(this.userEmail =='Admin@gmail.com' && this.userPassword == '123456'){
-      showSuccessToast("Admin Login Successfully");
-      localStorage.setItem('currentUser','23');
-      this.servive.isAuthenticated = true;
-      window.location.href='/Dashboard'
-      // this.router.navigate(['/Dashboard']);
-      this.servive.loader=false;
-    }else{
-      showDangerToast('InValid Credential');
-      this.servive.loader=false;
-    }
-    
- 
+    this.servive.loader = true;
     var val = { Username: this.userEmail, Password: this.userPassword };
-    this.servive.getLogin(val).subscribe((res) => {
-      this.response = res;
 
-      if (this.response['status_code'] == 100) {
-        this.UserList = JSON.parse(this.response['message'])[0];
+    this.servive.getLogin(val).subscribe(
+      (res) => {
+        this.response = res;
 
-        localStorage.setItem(
-          'currentUser',
-          JSON.stringify(this.UserList, this.replacer)
-        );
-        
-        this.servive.isAuthenticated = true;
-      } else {
-        console.log(this.response);
-
-        alert(this.response['message']);
+        if (this.response['status_code'] == 100) {
+          // Authentication successful
+          this.UserList = JSON.parse(this.response['message'])[0];
+          localStorage.setItem(
+            'currentUser',
+            JSON.stringify(this.UserList, this.replacer)
+          );
+          this.servive.isAuthenticated = true;
+          showSuccessToast("Login Successful");
+          this.router.navigate(['/Dashboard']); // Redirect to dashboard
+        } else {
+          // Authentication failed
+          showDangerToast('Invalid Credentials');
+        }
+      },
+      (error) => {
+        // Error handling
+        console.error('Error during authentication:', error);
+        showDangerToast('An error occurred during authentication');
       }
-    });
+    );
   }
 }
