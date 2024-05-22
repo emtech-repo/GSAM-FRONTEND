30e68b77435a8a7aa664dc5309251fcc6ae29dd6
ClintonKwaga <ochiengclinton01@gmail.com>
2024-05-22 10:25:06 +0300

    new

--- src/app/documents/retrieve/retrieve.component.ts
+++ src/app/documents/retrieve/retrieve.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-retrieve',
   templateUrl: './retrieve.component.html',
-  styleUrl: './retrieve.component.css'
+  styleUrls: ['./retrieve.component.css']
 })
-export class RetrieveComponent {
+export class RetrieveComponent implements AfterViewInit {
+  isModalVisible = false;
+  modalElement?: HTMLElement;
 
+  constructor(private renderer: Renderer2, private el: ElementRef, private cdr: ChangeDetectorRef) { }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.modalElement = this.el.nativeElement.querySelector('#myModal');
+      this.cdr.detectChanges(); // Trigger change detection
+    });
+  }
+
+  toggleModal() {
+    if (this.isModalVisible) {
+      this.hideModal();
+    } else {
+      this.showModal();
+    }
+  }
+
+  showModal() {
+    if (this.modalElement) {
+      this.renderer.addClass(this.modalElement, 'show');
+    }
+  }
+
+  hideModal() {
+    if (this.modalElement) {
+      this.renderer.removeClass(this.modalElement, 'show');
+    }
+  }
 }
