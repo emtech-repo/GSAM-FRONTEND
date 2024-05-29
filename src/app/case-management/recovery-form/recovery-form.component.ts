14de88adb28752e83dccd272b6ee040f349061c8
WendyNgiye <ngiyewendy2@gmail.com>
2024-05-29 16:32:13 +0300

    decision

--- src/app/case-management/recovery-form/recovery-form.component.ts
+++ src/app/case-management/recovery-form/recovery-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { ToastrService } from 'ngx-toastr';
 import { SharedService } from '../../shared.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recovery-form',
@@ -10,14 +11,58 @@ import { SharedService } from '../../shared.service';
 })
 export class RecoveryFormComponent {
 
+   loanAccount: string | null = null;
+    decisionDetails: any;
+     Assigneddata: any = {};
+
    constructor(
     private toastr: ToastrService,
     public bsModalRef: BsModalRef,
-    private sharedService: SharedService
+    private sharedService: SharedService,
+    private route: ActivatedRoute
+
   ) { }
+
+ ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.loanAccount = params['loanAccount'];
+      console.log('Received loan account:', this.loanAccount); // Log the received loan account
+      if (this.loanAccount) {
+        this.fetchDecisionDetails();
+      }
+    });
+  }
+
+  fetchDecisionDetails(): void {
+    this.sharedService.getCaseDetails(this.loanAccount!).subscribe(
+      (response: any) => {
+        if (response && response.result && Array.isArray(response.result)) {
+          const result = response.result;
+          // Find the case details object with matching loanAccount
+          const decisionDetail = result.find((caseItem: any) => caseItem.loanAccount === this.loanAccount);
+          if (decisionDetail) {
+            this.decisionDetails = decisionDetail;
+            console.log('Case details:', this.decisionDetails); // Console log the fetched case details
+          } else {
+            console.error('Case details not found for loan account:', this.loanAccount);
+          }
+        } else {
+          console.error('Invalid response format:', response);
+        }
+      },
+      (error: any) => {
+        console.error('Failed to fetch case details:', error);
+      }
+    );
+  }
+
+
+
 
   closeModal(): void {
     this.bsModalRef.hide();
   }
+
+
 
 }
